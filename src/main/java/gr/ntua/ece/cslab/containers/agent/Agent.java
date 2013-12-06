cd07361592ab5ea9ba76d0f86574596e3152f989
Giannis Giannakopoulos <ggian@cslab.ece.ntua.gr>
2013-12-06 16:15:42 +0200

    updated Agent struct

--- src/main/java/gr/ntua/ece/cslab/containers/agent/Agent.java
+++ src/main/java/gr/ntua/ece/cslab/containers/agent/Agent.java
@@ -6,11 +6,15 @@ public class Agent {
 	
 	private PreferenceList preferences;
 	private int id;
+	private AgentStatus status;
+	private int partnerId=-1;
 	
 	public Agent() {
 		this.setId(-1);
 		this.setPreferences(null);
 	}
+	
+	// getters and setters, nothing to look here ;)
 	
 	public PreferenceList getPreferences() {
 		return preferences;
@@ -20,6 +24,14 @@ public class Agent {
 		return id;
 	}
 
+	public AgentStatus getStatus() {
+		return status;
+	}
+
+	public int getPartnerId() {
+		return partnerId;
+	}
+	
 	public void setPreferences(PreferenceList preferences) {
 		this.preferences = preferences;
 	}
@@ -28,6 +40,16 @@ public class Agent {
 		this.id = id;
 	}
 
+	public void setStatus(AgentStatus status) {
+		this.status = status;
+	}
+
+	public void setPartnerId(int partnerId) {
+		this.partnerId = partnerId;
+	}
+
+	//	getters and setters stop
+	
 	public static void main(String[] args) {
 		
 	}
